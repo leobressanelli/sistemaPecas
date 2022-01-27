8eafe4e51e34d2b5804d83ef60ca258d683e6a84
Leonardo Ramos <leo.ramos1712@gmail.com>
2022-01-27 14:26:53 -0300

    Sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,27 @@
+let pesoPeca = 100
+
+if(pesoPeca > 100){
+console.log("peso maior que 100g, podemos cadastrar a peça")
+}
+else{
+    console.log("Peso insuficiente, não foi possível cadastrar")
+}
+
+let numeropecas = 10
+
+if(numeropecas < 10){
+    console.log("Peça cadastrada na caixa")
+}
+else{
+    console.log("Não há espaço disponível na caixa")
+}
+
+let nomePeca ="Motor"
+
+console.log("o comprimento do nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3){
+    console.log("Nome da peça muito curto, não pode cadastrar")
+} else{
+    console.log("Nome da peça adequado, podemos cadastrar")
+}
